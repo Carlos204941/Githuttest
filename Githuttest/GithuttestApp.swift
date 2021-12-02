9d4b328ab1f96ca4e9f5357862380406f08ef258
Carlos Martinez <calinmtzhdz3@icloud.com>
2021-12-01 22:44:36 -0500

    Initial Commit

---
+++ Githuttest/GithuttestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithuttestApp.swift
+//  Githuttest
+//
+//  Created by Carlos Martinez on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct GithuttestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
