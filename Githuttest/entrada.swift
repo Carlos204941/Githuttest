a82dd5e09c8728774987b19abd3825f76fbebf5b
Carlos Martinez <calinmtzhdz3@icloud.com>
2021-12-09 22:53:14 -0500

    otra vista

---
+++ Githuttest/entrada.swift
@@ -0,0 +1,20 @@
+//
+//  entrada.swift
+//  Githuttest
+//
+//  Created by Carlos Martinez on 2021-12-08.
+//
+
+import SwiftUI
+
+struct entrada: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct entrada_Previews: PreviewProvider {
+    static var previews: some View {
+        entrada()
+    }
+}
